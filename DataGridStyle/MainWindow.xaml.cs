41c8bbe6820750f717080dc2d029a15272fabd43
Daniel Koman <dankoman30@gmail.com>
2024-10-25 10:41:48 -0700

    initial design

---
+++ DataGridStyle/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataGridStyle
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class GridItem
+    {
+        public string Module { get; set; }
+        public string Item { get; set; }
+        public string Proxy { get; set; }
+        public string Account { get; set; }
+        public string Status { get; set; }
+    }
+}
