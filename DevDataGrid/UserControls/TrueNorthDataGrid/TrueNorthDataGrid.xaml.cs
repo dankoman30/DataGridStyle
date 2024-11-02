9c1ce31f6f4a50633e74c9982db6e3c468e427a1
Daniel Koman <dankoman30@gmail.com>
2024-11-02 14:08:27 -0700

    split out dependency props for colors

--- DevDataGrid/UserControls/TrueNorthDataGrid/TrueNorthDataGrid.xaml.cs
+++ DevDataGrid/UserControls/TrueNorthDataGrid/TrueNorthDataGrid.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace DevDataGrid.UserControls
 {
@@ -12,7 +13,7 @@ namespace DevDataGrid.UserControls
             InitializeComponent();
         }
 
-        // ItemsSource property
+        #region Original Properties
         public static readonly DependencyProperty ItemsSourceProperty =
             DependencyProperty.Register(
                 "ItemsSource",
@@ -32,7 +33,6 @@ namespace DevDataGrid.UserControls
             control.MainDataGrid.ItemsSource = e.NewValue as IEnumerable;
         }
 
-        // AutoGenerateColumns property
         public static readonly DependencyProperty AutoGenerateColumnsProperty =
             DependencyProperty.Register(
                 "AutoGenerateColumns",
@@ -52,10 +52,90 @@ namespace DevDataGrid.UserControls
             control.MainDataGrid.AutoGenerateColumns = (bool)e.NewValue;
         }
 
-        // Columns property
         public ObservableCollection<DataGridColumn> Columns
         {
             get { return MainDataGrid.Columns; }
         }
+        #endregion
+
+        #region Color Properties
+        public static readonly DependencyProperty BackgroundColorProperty =
+            DependencyProperty.Register(
+                "BackgroundColor",
+                typeof(Color),
+                typeof(TrueNorthDataGrid),
+                new PropertyMetadata(Color.FromRgb(18, 18, 18)));  // #FF121212
+
+        public Color BackgroundColor
+        {
+            get { return (Color)GetValue(BackgroundColorProperty); }
+            set { SetValue(BackgroundColorProperty, value); }
+        }
+
+        public static readonly DependencyProperty ForegroundColorProperty =
+            DependencyProperty.Register(
+                "ForegroundColor",
+                typeof(Color),
+                typeof(TrueNorthDataGrid),
+                new PropertyMetadata(Color.FromRgb(221, 221, 221)));  // #FFDDDDDD
+
+        public Color ForegroundColor
+        {
+            get { return (Color)GetValue(ForegroundColorProperty); }
+            set { SetValue(ForegroundColorProperty, value); }
+        }
+
+        public static readonly DependencyProperty HeaderBackgroundColorProperty =
+            DependencyProperty.Register(
+                "HeaderBackgroundColor",
+                typeof(Color),
+                typeof(TrueNorthDataGrid),
+                new PropertyMetadata(Color.FromRgb(107, 91, 197)));  // #FF6B5BC5
+
+        public Color HeaderBackgroundColor
+        {
+            get { return (Color)GetValue(HeaderBackgroundColorProperty); }
+            set { SetValue(HeaderBackgroundColorProperty, value); }
+        }
+
+        public static readonly DependencyProperty RowHoverColorProperty =
+            DependencyProperty.Register(
+                "RowHoverColor",
+                typeof(Color),
+                typeof(TrueNorthDataGrid),
+                new PropertyMetadata(Color.FromRgb(45, 45, 45)));  // #FF2D2D2D
+
+        public Color RowHoverColor
+        {
+            get { return (Color)GetValue(RowHoverColorProperty); }
+            set { SetValue(RowHoverColorProperty, value); }
+        }
+
+        public static readonly DependencyProperty AlternateRow1ColorProperty =
+            DependencyProperty.Register(
+                "AlternateRow1Color",
+                typeof(Color),
+                typeof(TrueNorthDataGrid),
+                new PropertyMetadata(Color.FromRgb(30, 30, 30)));  // #FF1E1E1E
+
+        public Color AlternateRow1Color
+        {
+            get { return (Color)GetValue(AlternateRow1ColorProperty); }
+            set { SetValue(AlternateRow1ColorProperty, value); }
+        }
+
+        public static readonly DependencyProperty AlternateRow2ColorProperty =
+            DependencyProperty.Register(
+                "AlternateRow2Color",
+                typeof(Color),
+                typeof(TrueNorthDataGrid),
+                new PropertyMetadata(Color.FromRgb(53, 53, 53)));  // #FF353535
+
+        public Color AlternateRow2Color
+        {
+            get { return (Color)GetValue(AlternateRow2ColorProperty); }
+            set { SetValue(AlternateRow2ColorProperty, value); }
+        }
+        #endregion
     }
 }
